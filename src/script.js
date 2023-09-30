392da5043aba18cc49217684d93f95359a6cea74
Ridwan-Lawal <lawal6382@gmail.com>
2023-09-30 11:05:27 +0100

    built and style the features styled section

--- src/script.js
+++ src/script.js
@@ -1,24 +1,41 @@
 "use strict";
 
-const nav = document.querySelector(".nav-bar");
+// const nav = document.querySelector(".nav-bar");
+const featuresType = document.querySelector(".features-type");
 
-// /////////////////// toggle menu
-const toggle = function (e, classname) {
-  e.target
-    .closest(".nav-container")
-    .nextElementSibling.classList.toggle(classname);
-};
+// // /////////////////// toggle menu
+// const toggle = function (e, classname) {
+//   e.target
+//     .closest(".nav-container")
+//     .nextElementSibling.classList.toggle(classname);
+// };
 
-nav.addEventListener("click", (e) => {
-  if (e.target.closest(".hamburger-box")) {
-    e.target.closest(".hamburger").classList.toggle("is-active");
-    toggle(e, "w-0");
-    toggle(e, "w-full");
-    toggle(e, "px-8");
-    toggle(e, "overflow-hidden");
-    document.querySelector(".logo").src = `./images/logo-bookmark.svg`;
-  }
-  if (e.target.closest(".is-active")) {
-    document.querySelector(".logo").src = `/images/logo2.svg`;
-  }
-});
+// nav.addEventListener("click", (e) => {
+//   if (e.target.closest(".hamburger-box")) {
+//     e.target.closest(".hamburger").classList.toggle("is-active");
+//     toggle(e, "w-0");
+//     toggle(e, "w-full");
+//     toggle(e, "px-8");
+//     toggle(e, "overflow-hidden");
+//     document.querySelector(".logo").src = `./images/logo-bookmark.svg`;
+//   }
+//   if (e.target.closest(".is-active")) {
+//     document.querySelector(".logo").src = `/images/logo2.svg`;
+//   }
+// });
+
+//######### features type
+// const featureLinks = document.querySelectorAll(".feature-link");
+// const featuresTabs = document.querySelectorAll(".tabs");
+
+// featuresType.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("feature-link")) {
+//     e.preventDefault();
+//     featureLinks.forEach((link) => link.classList.remove("border-b-4"));
+//     featuresTabs.forEach((tab) => tab.classList.add("hidden"));
+//     e.target.classList.add("border-b-4");
+//     const attrb = e.target.getAttribute("href");
+//     const eachTab = document.querySelector(attrb);
+//     eachTab.classList.remove("hidden");
+//   }
+// });
